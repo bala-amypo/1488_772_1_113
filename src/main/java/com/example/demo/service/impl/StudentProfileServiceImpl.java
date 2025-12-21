49c2f48de739810861f934898c72f20e640e8719
bala-amypo <bala@amypo.in>
2025-12-21 17:10:39 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentProfileServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/students")
-public class StudentProfileController {
+public class StudentProfileControllerImpl {
 
     private final StudentProfileService service;
 
